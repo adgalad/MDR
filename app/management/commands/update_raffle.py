37a36894b4f0c789d54abc6a15bc0067730a3eef
Carlos Spaggiari Roa <carlos.25896@gmail.com>
2018-08-09 13:43:51 -0400

    sintax error

--- app/management/commands/update_raffle.py
+++ app/management/commands/update_raffle.py
@@ -12,7 +12,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         while(True):
-            raffles = models.Raffle.object.filter(transaction__isnull=True)
+            raffles = models.Raffle.objects.filter(transaction__isnull=True)
             for raffle in raffles:
                 print(raffle.name)
                 raffle.getWinner()
