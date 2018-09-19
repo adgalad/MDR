0b1d0fb9ac8d202bb36dd01419b94688a0408c5f
Carlos Spaggiari Roa <carlos.25896@gmail.com>
2018-09-19 15:35:59 -0400

    plomo al hampa

--- app/models.py
+++ app/models.py
@@ -53,59 +53,75 @@ class Dash:
         (["-rpcuser="+RPC_USER] if RPC_USER else []) +
         (["-rpcpassword="+RPC_PASSWORD] if RPC_PASSWORD else []) +
          ["-testnet"])
-
+  
+  @staticmethod
   def createrawtransaction(output, addressAmount):
     return Dash.call(["createrawtransaction",json.dumps(output), json.dumps(addressAmount)])
   
+  @staticmethod  
   def createmultisig(signsRequired, pubkeys):
     return Dash.call(["createmultisig", str(signsRequired), json.dumps(pubkeys)])
-
+  
+  @staticmethod
   def dumpprivkey(address):
     return Dash.call(["dumpprivkey", address]).replace('\n','')    
-
+  
+  @staticmethod
   def getaddresstxids(addresses):
     return Dash.call(["getaddresstxids", json.dumps({"addresses":addresses})])
-
+  
+  @staticmethod
   def getaddressbalance(addresses):
     return {'received':100}
     return Dash.call(["getaddressbalance",json.dumps({'addresses':addresses})])
-
+  
+  @staticmethod
   def getblock(blockHash):
     return {'time':5515236123}
     return Dash.call(["getblock", blockHash])
-
+  
+  @staticmethod
   def getblockcount():
     count = Dash.call(['getblockcount'])
     return int(count) if count else 0
-
+  
+  @staticmethod
   def getblockhash(count):
     return Dash.call(["getblockhash", str(count)])
   
+  @staticmethod  
   def getnewaddress():
     return Dash.call(["getnewaddress"]).replace('\n','')
-
+  
+  @staticmethod
   def getrawtransaction(address, parsed=1):
     return Dash.call(["getrawtransaction", address, parsed])
-
+  
+  @staticmethod
   def sendrawtransaction(hexadecimalValue, allowhighfees=False, instantsend=False, bypasslimits=False):
     return Dash.call(["sendrawtransaction",hexadecimalValue, allowhighfees, instantsend, bypasslimits])  
   
+  @staticmethod  
   def sendtoaddress(address, amount):
     return Dash.call(["sendtoaddress", address, str(amount)])
-
+  
+  @staticmethod
   def signrawtransaction(transaction, output, privkeys):
     return Dash.call(["signrawtransaction",transaction, json.dumps(output), json.dumps(privkeys)])  
-
+  
+  @staticmethod
   def validateaddress(address):
     return Dash.call(['validateaddress', address])
-
+  
+  @staticmethod
   def verifymessage(address, signature, finalMessage):
     return Dash.call(["verifymessage", address, signature, finalMessage])
 
   
 
   
-
+  
+  @staticmethod
   def call(args):
     try:
       #print("Command: ", ' '.join(command))
--- raffle/settings.py
+++ raffle/settings.py
@@ -140,6 +140,54 @@ STATICFILES_DIRS = (
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 CKEDITOR_BASEPATH = os.path.join(STATIC_URL, "ckeditor/ckeditor/")
+CKEDITOR_CONFIGS = {
+    'default': {
+        'skin': 'moono',
+        # 'skin': 'office2013',
+        'toolbar_Basic': [
+            ['Source', '-', 'Bold', 'Italic']
+        ],
+        'toolbar_YourCustomToolbarConfig': [
+            {'name': 'document', 'items': ['Source', '-', 'Preview', 'Maximize']},
+            {'name': 'basicstyles',
+             'items': ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat']},
+            {'name': 'paragraph',
+             'items': ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', '-',
+                       'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock']},
+            {'name': 'styles', 'items': ['Styles', 'Format', 'Font', 'FontSize']},
+            {'name': 'colors', 'items': ['TextColor', 'BGColor']},
+            {'name': 'insert',
+             'items': ['Image', 'Youtube', 'Table','Link', 'Unlink', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak']},
+            {'name': 'about', 'items': ['About']},
+        ],
+        'toolbar': 'YourCustomToolbarConfig',  # put selected toolbar config here
+        # 'toolbarGroups': [{ 'name': 'document', 'groups': [ 'mode', 'document', 'doctools' ] }],
+        # 'height': 291,
+        # 'width': '100%',
+        # 'filebrowserWindowHeight': 725,
+        # 'filebrowserWindowWidth': 940,
+        # 'toolbarCanCollapse': True,
+        # 'mathJaxLib': '//cdn.mathjax.org/mathjax/2.2-latest/MathJax.js?config=TeX-AMS_HTML',
+        'tabSpaces': 4,
+        'extraPlugins': ','.join([
+            'uploadimage', # the upload image feature
+            # your extra plugins here
+            'div',
+            'autolink',
+            'autoembed',
+            'embedsemantic',
+            'autogrow',
+            # 'devtools',
+            'widget',
+            'lineutils',
+            'clipboard',
+            'dialog',
+            'dialogui',
+            'elementspath',
+            'youtube'
+        ]),
+    }
+}
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
@@ -150,3 +198,6 @@ EMAIL_HOST_PASSWORD = 'asd123asd'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'Mega Dash Raffle'
+
+
+
