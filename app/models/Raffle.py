f94da8bba04edff8601081d3495ceb546f1d8be2
Carlos Spaggiari Roa <carlos.25896@gmail.com>
2018-10-11 13:39:37 -0400

    removing complete sign

--- app/models/Raffle.py
+++ app/models/Raffle.py
@@ -260,8 +260,8 @@ class Raffle(models.Model):
 
     privkey = Dash.dumpprivkey(self.MSaddress)
     sign = Dash.signrawtransaction(transaction.replace('\n',''), outputs2, [privkey])
-    print(">>>>>>>>>>>>>>>>>", sign)
-    if not sign or not "complete" in sign or not sign['complete']:
+
+    if not sign:
       return -1
     
     self.commandSignRawTx = ' '.join('signrawtransaction', sign['hex'], outputs2, '[ "<b style="color:#990000">Your private key</b>" ]')
