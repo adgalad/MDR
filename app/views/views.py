64bb2b44dfceeb279a69c8119a092a0072e1f884
Carlos Spaggiari Roa <carlos.25896@gmail.com>
2018-10-15 12:37:40 -0400

    timezone

--- app/views/views.py
+++ app/views/views.py
@@ -8,6 +8,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from django.utils import timezone
 from app import models, forms
 from app.dash import Dash
 
