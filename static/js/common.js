3da50ffe7ca51c9bf072d983f185bc1ba85da4ed
Carlos Spaggiari Roa <carlos.25896@gmail.com>
2018-11-08 14:20:35 -0400

    $()

--- static/js/common.js
+++ static/js/common.js
@@ -93,7 +93,7 @@ var methods = {
     toastWrapAll  = (!$('.toast-container').length) ? $('<div></div>').addClass('toast-container').addClass('toast-position-' + localSettings.position).appendTo('body') : $('.toast-container');
     toastItemOuter  = $('<div></div>').addClass('toast-item-wrapper');
     toastItemInner  = $('<div></div>').hide().addClass('toast-item toast-type-' + localSettings.type).appendTo(toastWrapAll).html($('<p>').append (localSettings.text)).animate(localSettings.inEffect, localSettings.inEffectDuration).wrap(toastItemOuter);
-    toastItemClose  = $('<div></div>').addClass('toast-item-close').prependTo(toastItemInner).html(localSettings.closeText).click(function() { $().toastmessage('removeToast',toastItemInner, localSettings) });
+    toastItemClose  = $('<div></div>').addClass('toast-item-close').prependTo(toastItemInner).html(localSettings.closeText).click(function() { toastmessage('removeToast',toastItemInner, localSettings) });
     toastItemImage  = $('<div></div>').addClass('toast-item-image').addClass('toast-item-image-' + localSettings.type).prependTo(toastItemInner);
 
         if(navigator.userAgent.match(/MSIE 6/i))
