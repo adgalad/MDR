eeeb20e9e1cae17fc099dcf9157a6af0a248c318
Carlos Spaggiari Roa <carlos.25896@gmail.com>
2018-11-18 16:05:03 -0400

    update_raffle pid

--- app/management/commands/update_raffle.py
+++ app/management/commands/update_raffle.py
@@ -1,16 +1,38 @@
 #!/usr/bin/env python
 import time
+import os
 
 from django.contrib.auth.models import Permission, Group, ContentType
 from django.core.management.base import BaseCommand
 
 import app.models as models
 
+pidfile = ""
+
 class Command(BaseCommand):
 
     DELAY = 10
+   
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--pid',
+            help='Specify pid file to use when running as daemon.',
+        )
+
+    def writePidFile(self):
+        pid = str(os.getpid())
+        f = open(pidfile, 'w')
+        f.write(pid)
+        f.close()
+
 
     def handle(self, *args, **options):
+
+        if options['pid'] is not None:
+            global pidfile
+            pidfile = options['pid']
+            self.writePidFile()
+
         while(True):
             try:
                 # Check if a raffle finished
@@ -29,8 +51,10 @@ class Command(BaseCommand):
                 print(e)
             time.sleep(self.DELAY)
 
+def exit():
+    if pidfile != "":
+        os.remove(pidfile)
 
 
-    
-    
-    
+import atexit
+atexit.register(exit)
