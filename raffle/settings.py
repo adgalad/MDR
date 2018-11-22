f8f03067f577084a012e727799f73e9f8cddb4f9
Carlos Spaggiari Roa <carlos.25896@gmail.com>
2018-11-22 10:49:44 -0400

    debug false

--- raffle/settings.py
+++ raffle/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
 # Redirect to HTTPS
-# SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = True
 
 ALLOWED_HOSTS = ['*']
 
