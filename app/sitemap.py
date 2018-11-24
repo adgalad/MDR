a0b4ee9a94f6574e35feb5a92bcf1b47ff3e2cc6
Carlos Spaggiari Roa <carlos.25896@gmail.com>
2018-11-24 18:03:03 -0400

    __isnull

--- app/sitemap.py
+++ app/sitemap.py
@@ -6,7 +6,7 @@ class Raffle(Sitemap):
     priority = 0.5
 
     def items(self):
-        return models.Raffle.objects.filter(transaction_isnull=True)
+        return models.Raffle.objects.filter(transaction__isnull=True)
 
     def lastmod(self, obj):
         return obj.lastmod
